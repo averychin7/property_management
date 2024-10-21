2fa3c8809d3792feb93064cf2eb53c3f530a4910
averychin7 <avery.chin@tbcollege.com>
2024-10-21 15:41:02 -0400

    setup: body-parser and cors

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
